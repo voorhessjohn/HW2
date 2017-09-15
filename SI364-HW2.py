453e2bde64eaffe9c2eb0070ce732fd96b46150a
jczetta <jczetta@gmail.com>
2017-09-14 22:59:48 -0400

    Clarifications

--- SI364-HW2.py
+++ SI364-HW2.py
@@ -30,8 +30,9 @@ if __name__ == '__main__':
 # - should include an HTML form (of any kind: text entry, radio button, checkbox... feel free to try out whatever you want)
 # - should, on submission of data to the HTML form, show new data that depends upon the data entered into the submission form (text entered, radio button selected, etc). So if a user has to enter a number, it should do an operation on that number. If a user has to select a radio button representing a song name, it should do a search for that song in an API.
 # You should feel free to be creative and do something fun for you -- 
-# And use this opportunity to make sure you understand these steps if you think going slowly and carefully writing out steps for a simpler data transaction will help build your understanding!
-# You can assume that a user will give you the type of input/response you expect; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do so.)
+# And use this opportunity to make sure you understand these steps: if you think going slowly and carefully writing out steps for a simpler data transaction, like Problem 1, will help build your understanding, you should definitely try that!
+
+# You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
 
 
 
